0d5ce47ffb27c85ac0b347dc88cca0f057a838fe
Lennart Meyer <lennartmeyer@lennarts-mbp.home>
2018-08-02 23:14:07 +0200

    Initial setup@

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('HELLO')
+})
+
+app.listen(process.env.PORT || 8081)
